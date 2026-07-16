487ecc48b9d3a43da78da234cb2ad8ff5daeb05f
horrorbringer <vannysmilekh@gmail.com>
2026-07-16 10:17:57 +0700

    Return JSON response for root route instead of welcome view

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'name' => config('app.name'),
+        'status' => 'ok',
+        'admin' => url('/admin'),
+        'api' => url('/api'),
+    ]);
 });
